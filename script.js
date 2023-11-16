7c33b976fa6121c0a6a3a101fd612c85922d3506
atiks88 <141978852+atiks88@users.noreply.github.com>
2023-11-16 16:06:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+ function incrementCounter() {
+   
+        const counterElement = document.getElementById('counter');
+        const currentValue = parseInt(counterElement.innerText);
+
+        alert("Un-incremented Value: " + currentValue);
+
+        counterElement.innerText = currentValue + 1;
+    }
